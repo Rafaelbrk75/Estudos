ab4864215c0c2d2542e64af532ea7cf0a5fa5e9c
Rafaelbrk75 <rafaelbrk75@gmail.com>
2024-09-03 17:00:05 -0300

    atualização no array

---
+++ array/aula-36/matrizes.js
@@ -0,0 +1,34 @@
+const arr = [ 
+  "1 Nível",
+  ["2 Nível", 42, true],
+  [
+    ["3 nível 1 item", "Olá mundo!"],
+    ["3 nível, 2 item", "Oi, mundo!"],
+  ],
+  []
+
+] 
+
+console.log(arr)
+console.log[arr[0]]
+console.log[arr[1]]
+console.log[arr[1][0]] //Será mostrado a posição 0/inícial do 1 array.
+// Cada par de colchetes referencia um nível mais interno do array
+console.log(arr[1][1])    // Saída: 42
+console.log(arr[2])       // Saída: [ [ '3º nível, 1º item', 'Olá, mundo!' ], [ '3º nível, 2º item', 'Oi, mundo!' ] ]
+console.log(arr[2][0])    // Saída: [ '3º nível, 1º item', 'Olá, mundo!' ]
+console.log(arr[2][1][1]) // Saída: 'Oi, mundo!'
+
+// Podemos ver claramente a estrutura de uma matriz em uma tabela
+const matriz = [
+    ["l1, c1", "l1, c2", "l1, c3", "l1, c4"],
+    ["l2, c1", "l2, c2", "l2, c3", "l2, c4"],
+    ["l3, c1", "l3, c2", "l3, c3", "l3, c4"],
+  ]
+
+console.table(matriz)
+
+matriz.push(["Nova linha"])
+matriz[0].push("Nova coluna")
+
+console.table(matriz)
