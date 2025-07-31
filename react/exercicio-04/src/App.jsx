dd0f7fbac60a2207138a9ec1c600fb0061278697
Rafael Rodrigues <rafaelbrk75@gmail.com>
2025-07-31 13:26:51 -0300

    subindo arquivos

--- react/exercicio-04/src/App.jsx
+++ react/exercicio-04/src/App.jsx
@@ -1,14 +1,18 @@
 import { useState } from "react";
+import Input from "./components/Input";
 
 function App() {
   const [password, setPassword] = useState("");
   const [copyText, setCopyText] = useState("Copiar")
+  const [customSize, setCustomSize] = useState(12)
+  const [showInput, setShowInput] = useState(false)
+
+  const passwordSize = showInput ? customSize : 8
 
   function generate() {
     const characters = "'1234567890-=!@#$%¨&*()_+qwertyuiop[asdfghjklç~]zxcvbnm,.;/QWERTYUIOP{ASDFGHJKLÇ^}ZXCVBNM<>:?"
-    const length = 12
     let newPassword = ""
-    for (let i = 0; i < length; i++){
+    for (let i = 0; i < passwordSize; i++){
       const position = Math.floor(Math.random() * characters.length)
       newPassword += characters[position]
     }
@@ -24,7 +28,22 @@ function App() {
   return (
     <div className="App">
       <h1>Gerador de Senhas</h1>
-      <button onClick={generate}>Gerar</button>
+      <div>
+        <label htmlFor="showInput">Customizar tamanho:</label>
+        <input 
+        type="checkbox"
+        id="showInput"
+        value={showInput}
+        onChange={() => setShowInput(currentState => !currentState)}
+        />
+      </div>
+      {showInput ? (
+        <div>
+        <label htmlFor="passwordSize" >Tamanho:</label>
+        <Input passwordSize={customSize} setPasswordSize={setCustomSize} />
+      </div>
+      ) : null}
+      <button onClick={generate}>Gerar senha de {passwordSize} caracteres!</button>
       <button onClick={copyToClipBoard}>{copyText}</button>
       <div>
         {password}
